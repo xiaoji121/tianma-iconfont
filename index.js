2511be3d93c8fc12b92ba406a57f4bf0cb16ad92
xiaoji121 <dong.fly.ming@gmail.com>
2014-09-24 21:25:25 +0800

    fix bug

--- index.js
+++ index.js
@@ -6,14 +6,18 @@
 
 var Path = require('path');
 
-module.exports = function (req, res) {
-    req(function (err) {
-        var baseName = Path.basename(req.path);
-        var pattern = /\.(eot|ttf|woff|svg)/i;
-        if (pattern.test(baseName)) {
-            res.head('Access-Control-Allow-Origin', '*');
-            res.type('application/octet-stream')
-        }
-        res(err);
-    });
+module.exports = function () {
+
+    return function(req, res) {
+        req(function (err) {
+            var baseName = Path.basename(req.path);
+            var pattern = /\.(eot|ttf|woff|svg)/i;
+            if (pattern.test(baseName)) {
+                res.head('Access-Control-Allow-Origin', '*');
+                res.type('application/octet-stream')
+            }
+            res(err);
+        });
+    }
+
 };
